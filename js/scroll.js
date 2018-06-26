c1ed586b237f0c5cfe9966d84fed8eb6fd319b25
Ben <benjii.goodman@hotmail.com>
2018-06-26 15:56:28 +0100

    changed layout to scroll in different directions, added portfolio page

--- js/scroll.js
+++ js/scroll.js
@@ -11,14 +11,15 @@ $(document).ready(function(){
       var hash = this.hash;
 
       // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    } // End if
+    }
   });
 });
+
+window.onload = function() {
+  document.getElementById('home').scrollIntoView();
+};

cab2418aff0ce621a91f269f57b9e2c53afb6518
Ben <benjii.goodman@hotmail.com>
2018-07-02 12:32:08 +0100

    Added project animation

--- js/scroll.js
+++ js/scroll.js
@@ -12,13 +12,6 @@ $(document).ready(function(){
     }
   });
 
-  // Add animation to skill bars
-  $('.skillbar').each(function(){
-		$(this).find('.skillbar-bar').animate({
-			width:$(this).attr('data-percent')
-		},6000);
-	});
-
   // Add functionality for slider
 });
 
@@ -26,4 +19,24 @@ $(window).on('load', function() {
   document.getElementById('home').scrollIntoView();
   // Initialise page to home pane
   $('#home-button').trigger('click');
+});
+
+// Animate computer svg
+var tl = new TimelineMax();
+$('.arrow-up, #portfolio-button').on('click', function() {
+  var tl = new TimelineMax();
+
+  tl.from('#stand', 0.5, {scaleY: 0, transformOrigin: "bottom", ease: Power2.easeOut})
+    .from('#standBack', 0.5, {scaleY: 0, transformOrigin: "bottom", ease: Bounce.easeOut}) 
+    .from('#monitorBottom', 0.7, {scaleX: 0, transformOrigin: "center", ease: Bounce.easeOut})
+    .from('#screen', 0.6, {scaleY: 0, transformOrigin: "bottom", ease: Circ.easeOut, delay: 0.4})
+    .from('#yellowBox', 0.5, {scale: 0})
+    .staggerFrom('#Layer_1 > g:nth-child(1) > g path', 0.2, {scaleX: 0}, -0.1)
+
+  // Add animation to skill bars
+  $('.skillbar').each(function(){
+		$(this).find('.skillbar-bar').animate({
+			width:$(this).attr('data-percent')
+		},6000);
+  });
 });
